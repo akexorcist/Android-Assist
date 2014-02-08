5d2d9a5b96e2277c214fa5732e12a55433c1538b
akexorcist <akexorcist@gmail.com>
2014-02-09 02:25:47 +0700

    Nothing

---
+++ src/app/akexorcist/assist/AssistActivity.java
@@ -0,0 +1,12 @@
+package app.akexorcist.assist;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AssistActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.assist);
+	}
+}
